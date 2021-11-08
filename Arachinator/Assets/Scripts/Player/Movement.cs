822864404e769c2a5f9d1073e366786e2db6d110
Lucas Teles <lucas.teles@outlook.com>
2021-11-08 10:34:07 -0300

    WIP

--- Arachinator/Assets/Scripts/Player/Movement.cs
+++ Arachinator/Assets/Scripts/Player/Movement.cs
@@ -14,12 +14,6 @@ public class Movement : MonoBehaviour
 	void FixedUpdate ()
     {
         if (isLocked) return;
-        // rb.AddForce(velocity, ForceMode.VelocityChange);
-        // rb.velocity = new Vector3(
-        //     Mathf.Clamp(rb.velocity.x, -maxVelocity.x, maxVelocity.x),
-        //     rb.velocity.y,
-        //     Mathf.Clamp(rb.velocity.z, -maxVelocity.y, maxVelocity.y)
-        // );
         rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
     }
 
