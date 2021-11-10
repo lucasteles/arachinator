77b4ae727973d77e1a5e4d61ae578d5d3925a430
Lucas Teles <lucas.teles@outlook.com>
2021-11-09 23:36:33 -0300

    WIP

--- Arachinator/Assets/Scripts/Player/Player.cs
+++ Arachinator/Assets/Scripts/Player/Player.cs
@@ -7,29 +7,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, IDamageble
 {
+    [Header("Params")]
     [SerializeField]float speed = 5;
-    [SerializeField]PlayerHealthPointsUi ui;
-
     [SerializeField]float damageTime = .5f;
     [SerializeField]float damageFlashTime = 1f;
-    [SerializeField]Material damageMaterial;
-    [SerializeField]Material dissolveMaterial;
 
+    [Header("Audio")]
     [SerializeField]AudioClip hit;
     [SerializeField]AudioClip dieSound;
+
+    [Header("Refs")]
+    [SerializeField]PlayerHealthPointsUi ui;
 
     Movement movement;
     Rigidbody rb;
     Life life;
-    Renderer[] renderers;
-    Dictionary<Renderer, Material> originalMaterials;
-    Dictionary<Renderer, Material> damageMaterialsCache;
-    Dictionary<Renderer, Material> dissolveMaterialsCache;
     AudioSource audioSource;
-
-    Coroutine currentDamageCoroutine = null;
-    static readonly int FresnelLevel = Shader.PropertyToID("_FresnelLevel");
-    static readonly int DissolveLevel = Shader.PropertyToID("_Dissolve");
+    PlayerEffects playerEffects;
+    Coroutine currentDamageCoroutine;
 
     public bool IsInvincible { get; private set; }
 
@@ -38,8 +33,8 @@ public class Player : MonoBehaviour, IDamageble
         movement = GetComponent<Movement>();
         rb = GetComponent<Rigidbody>();
         life = GetComponent<Life>();
+        playerEffects = GetComponent<PlayerEffects>();
         audioSource = GetComponent<AudioSource>();
-        renderers = GetComponentsInChildren<Renderer>().ToArray();
         life.onDeath += OnDeath;
     }
     void OnDestroy() => life.onDeath -= OnDeath;
@@ -49,12 +44,6 @@ public class Player : MonoBehaviour, IDamageble
     void Start()
     {
         ui.SetMaxHealth(life.MaxLife);
-        damageMaterialsCache = InstantiateMaterials(damageMaterial);
-        dissolveMaterialsCache = InstantiateMaterials(dissolveMaterial);
-
-        originalMaterials =
-            renderers.Select(r => (r, r.sharedMaterial))
-                .ToDictionary(x => x.r, x => x.sharedMaterial);
     }
 
     void Update ()
@@ -90,29 +79,6 @@ public class Player : MonoBehaviour, IDamageble
         TakeDamage(amount);
     }
 
-    Dictionary<Renderer, Material> InstantiateMaterials(Material material)
-    {
-        var cache = new Dictionary<Renderer, Material>();
-        for (var i = 0; i < renderers.Length; i++)
-        {
-            var renderer = renderers[i];
-            var color = renderer.sharedMaterial.color;
-            var tex = renderer.sharedMaterial.mainTexture;
-            var newMaterial = new Material(material);
-            cache.Add(renderer, newMaterial);
-            renderer.sharedMaterial = newMaterial;
-            renderer.sharedMaterial.color = color;
-            renderer.sharedMaterial.mainTexture = tex;
-        }
-        return cache;
-    }
-
-    void RestoreMaterials()
-    {
-        for (var i = 0; i < renderers.Length; i++)
-            renderers[i].sharedMaterial = originalMaterials[renderers[i]];
-    }
-
     IEnumerator DieAnimation()
     {
         EnableInvicible();
@@ -140,11 +106,11 @@ public class Player : MonoBehaviour, IDamageble
         yield return new WaitForSeconds(.3f);
 
         var dissolveStep = 0.0065f;
-        yield return DissolveEffect(dissolveStep);
+        yield return playerEffects.DissolveEffect(dissolveStep);
 
         rb.MovePosition(Vector3.zero);
         transform.rotation = originalRotation;
-        yield return RestoreEffect(dissolveStep);
+        yield return playerEffects.DissolveRestoreEffect(dissolveStep);
 
         gun.enabled = webPistol.enabled = true;
         rb.constraints = constraints;
@@ -154,68 +120,24 @@ public class Player : MonoBehaviour, IDamageble
         movement.Unlock(lockKey);
     }
 
-    public void SetMaterialFromCache(Dictionary<Renderer, Material> cache)
-    {
-        for (var i = 0; i < renderers.Length; i++)
-        {
-            var renderer = renderers[i];
-            var newMaterial = cache[renderer];
-            renderer.sharedMaterial = newMaterial;
-        }
-    }
-
-    public void UseFresnelShader() => SetMaterialFromCache(damageMaterialsCache);
-    public void UseDissolveShader() => SetMaterialFromCache(dissolveMaterialsCache);
-    public void SetDissolveOnAllMaterials(float dissolve)
-    {
-        foreach (var mat in dissolveMaterialsCache.Values)
-            mat.SetFloat(DissolveLevel, dissolve);
-    }
-    public void SetFresnelOnAllMaterials(float dissolve)
-    {
-        foreach (var mat in damageMaterialsCache.Values)
-            mat.SetFloat(FresnelLevel, dissolve);
-    }
-
-    public IEnumerator DissolveEffect(float step)
-    {
-        UseDissolveShader();
-        for (var i = 0f; i <= 1; i+=step)
-        {
-            SetDissolveOnAllMaterials(i);
-            yield return null;
-        }
-    }
-
-    public IEnumerator RestoreEffect(float step)
-    {
-        for (var i = 1f; i >= 0; i-=step)
-        {
-            SetDissolveOnAllMaterials(i);
-            yield return null;
-        }
-
-        RestoreMaterials();
-    }
-
     IEnumerator DamageFlash()
     {
         const float initialFresnel = 2.5f;
         const float lastFresnel = 0.5f;
         var time = 0f;
-        UseFresnelShader();
-        SetFresnelOnAllMaterials(1f);
+        playerEffects.UseFresnelShader();
+        playerEffects.SetFresnelOnAllMaterials(1f);
 
         while (time <= damageFlashTime)
         {
             var tstep = Mathf.InverseLerp(0f, damageFlashTime, time);
             var level = Mathf.Lerp(initialFresnel, lastFresnel, tstep);
-            SetFresnelOnAllMaterials(level);
+            playerEffects.SetFresnelOnAllMaterials(level);
 
             time += Time.deltaTime;
             yield return null;
         }
-        RestoreMaterials();
+        playerEffects.RestoreMaterials();
         currentDamageCoroutine = null;
     }
 
---
+++ Arachinator/Assets/Scripts/Player/PlayerEffects.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Ui.HealthPoints;
+using UnityEngine;
+
+public class PlayerEffects : MonoBehaviour
+{
+    [SerializeField]Material damageMaterial;
+    [SerializeField]Material dissolveMaterial;
+
+    Renderer[] renderers;
+    Dictionary<Renderer, Material> originalMaterials;
+    Dictionary<Renderer, Material> damageMaterialsCache;
+    Dictionary<Renderer, Material> dissolveMaterialsCache;
+    static readonly int FresnelLevel = Shader.PropertyToID("_FresnelLevel");
+    static readonly int DissolveLevel = Shader.PropertyToID("_Dissolve");
+
+    void Awake()
+    {
+        renderers = GetComponentsInChildren<Renderer>().ToArray();
+    }
+
+    void Start()
+    {
+        damageMaterialsCache = InstantiateMaterials(damageMaterial);
+        dissolveMaterialsCache = InstantiateMaterials(dissolveMaterial);
+
+        originalMaterials =
+            renderers.Select(r => (r, r.sharedMaterial))
+                .ToDictionary(x => x.r, x => x.sharedMaterial);
+    }
+
+    Dictionary<Renderer, Material> InstantiateMaterials(Material material)
+    {
+        var cache = new Dictionary<Renderer, Material>();
+        for (var i = 0; i < renderers.Length; i++)
+        {
+            var renderer = renderers[i];
+            var color = renderer.sharedMaterial.color;
+            var tex = renderer.sharedMaterial.mainTexture;
+            var newMaterial = new Material(material);
+            cache.Add(renderer, newMaterial);
+            renderer.sharedMaterial = newMaterial;
+            renderer.sharedMaterial.color = color;
+            renderer.sharedMaterial.mainTexture = tex;
+        }
+        return cache;
+    }
+
+    public void RestoreMaterials()
+    {
+        for (var i = 0; i < renderers.Length; i++)
+            renderers[i].sharedMaterial = originalMaterials[renderers[i]];
+    }
+
+    public void SetMaterialFromCache(Dictionary<Renderer, Material> cache)
+    {
+        for (var i = 0; i < renderers.Length; i++)
+        {
+            var renderer = renderers[i];
+            var newMaterial = cache[renderer];
+            renderer.sharedMaterial = newMaterial;
+        }
+    }
+
+    public void UseFresnelShader() => SetMaterialFromCache(damageMaterialsCache);
+    public void UseDissolveShader() => SetMaterialFromCache(dissolveMaterialsCache);
+
+    public void SetDissolveOnAllMaterials(float dissolve)
+    {
+        foreach (var mat in dissolveMaterialsCache.Values)
+            mat.SetFloat(DissolveLevel, dissolve);
+    }
+
+    public void SetFresnelOnAllMaterials(float dissolve)
+    {
+        foreach (var mat in damageMaterialsCache.Values)
+            mat.SetFloat(FresnelLevel, dissolve);
+    }
+
+    public IEnumerator DissolveEffect(float step)
+    {
+        UseDissolveShader();
+        for (var i = 0f; i <= 1; i+=step)
+        {
+            SetDissolveOnAllMaterials(i);
+            yield return null;
+        }
+    }
+
+    public IEnumerator DissolveRestoreEffect(float step)
+    {
+        for (var i = 1f; i >= 0; i-=step)
+        {
+            SetDissolveOnAllMaterials(i);
+            yield return null;
+        }
+
+        RestoreMaterials();
+    }
+
+}
