33bea10804b9c481112bc8bc66c245086db69520
Rafael Delboni <rafadelboni@gmail.com>
2021-11-25 23:31:16 -0300

    feat: Adds tutorial

--- Arachinator/Assets/Scripts/EnableWebPistol.cs
+++ Arachinator/Assets/Scripts/EnableWebPistol.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.Ui.WebTutorial;
 using UnityEngine;
 
 public class EnableWebPistol : MonoBehaviour
 {
     [SerializeField] AudioClip audio;
+    WebTutorial tutorialUI;
+
+    private void Awake() =>
+        tutorialUI = FindObjectOfType<WebTutorial>();
 
     void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
+        
+        tutorialUI.Show();
         other.GetComponentInChildren<WebPistol>().enabled = true;
         CameraAudioSource.Instance.AudioSource.PlayOneShot(audio);
         Destroy(gameObject);
