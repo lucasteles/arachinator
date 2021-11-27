b8af2a82bf0cee42639fdcb09af4ecc70e44f239
Rafael Delboni <rafadelboni@gmail.com>
2021-11-27 14:54:26 -0300

    feat: Adds loading screen

--- Arachinator/Assets/Scripts/Menu/MainMenu.cs
+++ Arachinator/Assets/Scripts/Menu/MainMenu.cs
@@ -1,9 +1,50 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;  
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame() => SceneManager.LoadScene("level-1");
+    [SerializeField] private GameObject LoadingScreen;
+    [SerializeField] private TextMeshProUGUI LoadingText;
+
+    public void PlayGame()
+    {
+        GetComponent<Canvas>().enabled = false;
+        LoadingScreen.SetActive(true);
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+    
+        //Begin to load the Scene you specify
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("level-1");
+        //Don't let the Scene activate until you allow it to
+        asyncOperation.allowSceneActivation = false;
+        Debug.Log("Pro :" + asyncOperation.progress);
+        //When the load is still in progress, output the Text and progress bar
+        while (!asyncOperation.isDone)
+        {
+            //Output the current progress
+            LoadingText.text = "Loading progress: " + (asyncOperation.progress * 100) + "%";
+    
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+                //Change the Text to show the Scene is ready
+                LoadingText.text = "Press the space bar to continue";
+                //Wait to you press the space key to activate the Scene
+                if (Input.GetKeyDown(KeyCode.Space))
+                    //Activate the Scene
+                    asyncOperation.allowSceneActivation = true;
+            }
+    
+            yield return null;
+        }
+    }
+
     public void CloseGame()
     {
         #if UNITY_EDITOR
