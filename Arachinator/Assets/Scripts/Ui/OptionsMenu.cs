333e80f0e5b476dd26c2a4ad2ee3eca866e809a8
Rafael Delboni <rafadelboni@gmail.com>
2021-11-28 15:37:19 -0300

    feat: Options menu actually changes volume

---
+++ Arachinator/Assets/Scripts/Ui/OptionsMenu.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField] AudioMixer musicMixer;
+    [SerializeField] AudioMixer sfxMixer;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+    Gun gun;
+
+    private void SetVolume(AudioMixer mixer, float volume, bool sfx)
+    {
+        print(mixer.name + " " + volume);
+        mixer.SetFloat("MasterVolume", volume);
+        if (sfx) gun.StartShoot();
+    }
+
+    private void Awake()
+    {
+        gun = FindObjectOfType<Gun>();
+        gun.canShoot = false;
+        musicSlider.onValueChanged.AddListener(volume => SetVolume(musicMixer, volume, false));
+        sfxSlider.onValueChanged.AddListener(volume => SetVolume(sfxMixer, volume, true));
+    }
+
+    private void Start()
+    {
+        SetVolume(musicMixer, musicSlider.value, false);
+        SetVolume(sfxMixer, sfxSlider.value, false);
+    }
+}
