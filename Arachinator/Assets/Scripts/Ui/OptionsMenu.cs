228a68797fff03dd71deb380f4060fc88adacf7b
Lucas Teles <lucas.teles@outlook.com>
2021-11-28 17:08:07 -0300

    WIP

--- Arachinator/Assets/Scripts/Ui/OptionsMenu.cs
+++ Arachinator/Assets/Scripts/Ui/OptionsMenu.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -9,7 +11,10 @@ public class OptionsMenu : MonoBehaviour
     [SerializeField] Slider musicSlider;
     [SerializeField] Slider sfxSlider;
     [SerializeField] Slider qualitySlider;
+    [SerializeField] TMP_Dropdown resDropdown;
     Gun gun;
+
+    Resolution[] resolutions;
 
     void SetVolume(AudioMixer mixer, float volume, bool sfx)
     {
@@ -33,9 +38,28 @@ public class OptionsMenu : MonoBehaviour
         qualitySlider.value = QualitySettings.GetQualityLevel();
         musicSlider.value = GetVolume(musicMixer);
         sfxSlider.value = GetVolume(sfxMixer);
-
         musicSlider.onValueChanged.AddListener(volume => SetVolume(musicMixer, volume, false));
         sfxSlider.onValueChanged.AddListener(volume => SetVolume(sfxMixer, volume, true));
         qualitySlider.onValueChanged.AddListener(value => QualitySettings.SetQualityLevel((int)value));
+
+        resolutions = Screen.resolutions;
+        resDropdown.ClearOptions();
+        var currentResolutionIndex =
+            resolutions.ToList()
+                .FindIndex(x => x.height == Screen.currentResolution.height
+                                             && x.width == Screen.currentResolution.width);
+        var options = resolutions.Select(x => $"{x.width}x{x.height}").ToList();
+        resDropdown.AddOptions(options);
+        resDropdown.value = currentResolutionIndex;
+        resDropdown.RefreshShownValue();
+        resDropdown.onValueChanged.AddListener(SetResolution);
     }
+
+    void SetResolution(int i)
+    {
+        var res = resolutions[i];
+        print($"Setting resolution to {res.width}x{res.height}");
+        Screen.SetResolution(res.width,res.height, Screen.fullScreen);
+    }
+
 }
